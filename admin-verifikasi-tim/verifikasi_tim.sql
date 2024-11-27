3d2cc0e27e8f544aeff7f126fbf60662c0c5a650
Adrian Pratama Sasmita <adriansasmita2@gmail.com>
2024-11-28 02:15:22 +0700

    update verifikasi tim dan feedback dosen

--- admin-verifikasi-tim/verifikasi_tim.sql
+++ admin-verifikasi-tim/verifikasi_tim.sql
@@ -1,9 +1,8 @@
--- Trigger status
 CREATE OR REPLACE TRIGGER before_verify_proposal
 BEFORE UPDATE OF status ON proposals
 FOR EACH ROW
 BEGIN
-    IF :NEW.status = 'DIVERIFIKASI' AND :OLD.status != 'DIUSULKAN' THEN
+    IF (:NEW.status = 'DIVERIFIKASI' OR :NEW.status = 'VERIFIKASI_GAGAL') AND :OLD.status != 'DIUSULKAN' THEN
         RAISE_APPLICATION_ERROR(-20002, 'Proposal hanya dapat diverifikasi jika status sebelumnya DIUSULKAN.');
     END IF;
 END;
@@ -11,12 +10,18 @@ END;
 
 -- Procedure admin verifikasi tim
 CREATE OR REPLACE PROCEDURE verify_team (
-    p_proposals_id IN CHAR
+    p_proposals_id IN CHAR,
+    p_new_status   IN VARCHAR2
 ) AS
 BEGIN
+    -- Status
+    IF p_new_status NOT IN ('DIVERIFIKASI', 'VERIFIKASI_GAGAL') THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Status tidak valid.');
+    END IF;
+
     -- Update status proposal menjadi DISETUJUI
     UPDATE proposals
-    SET status = 'DIVERIFIKASI'
+    SET status = p_new_status
     WHERE id = p_proposals_id;
 
     -- Cek apakah ada baris yang diupdate
@@ -28,3 +33,10 @@ BEGIN
     COMMIT;
 END verify_team;
 /
+
+BEGIN
+    verify_team(
+        p_proposals_id => 'prop-001',
+        p_new_status => 'VERIFIKASI_GAGAL'
+    );
+END;
--- feedback-dosen/feedback_dosen.sql
+++ feedback-dosen/feedback_dosen.sql
@@ -1,10 +1,17 @@
+CREATE SEQUENCE feedback_seq
+START WITH 1  -- Nilai awal
+INCREMENT BY 1  -- Langkah kenaikan
+NOCACHE;        -- Tidak menggunakan cache untuk nilai
+
 CREATE OR REPLACE TRIGGER check_user_role_before_feedback
 BEFORE INSERT ON feedback
 FOR EACH ROW
 DECLARE
-    v_user_role VARCHAR2(50);
+    v_user_role     VARCHAR2(50);
+    v_lecture_nip   VARCHAR2(18);  -- Mendeklarasikan variabel nip dosen
+    v_feedback_nip  VARCHAR2(18);  -- Variabel untuk menyimpan nip dosen pemberi feedback
 BEGIN
-    -- Ambil role user dari tabel user
+    -- Ambil role user dari tabel "user"
     SELECT role
     INTO v_user_role
     FROM "user"
@@ -13,6 +20,23 @@ BEGIN
     -- Periksa apakah role user adalah 'lecture'
     IF v_user_role != 'lecture' THEN
         RAISE_APPLICATION_ERROR(-20003, 'Hanya user dengan role "lecture" yang dapat memberikan feedback.');
+    END IF;
+
+    -- Ambil nip dari dosen yang terkait dengan proposal
+    SELECT lecture_nip
+    INTO v_lecture_nip
+    FROM proposals
+    WHERE id = :NEW.proposals_id;
+
+    -- Ambil nip dosen yang memberikan feedback
+    SELECT nip
+    INTO v_feedback_nip
+    FROM lecture
+    WHERE user_id = :NEW.user_id;
+
+    -- Periksa apakah nip dosen pemberi feedback tidak sama dengan nip dosen di proposal
+    IF v_feedback_nip = v_lecture_nip THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Dosen yang memberikan feedback tidak boleh sama dengan dosen pendamping.');
     END IF;
 END;
 /
@@ -23,7 +47,8 @@ CREATE OR REPLACE PROCEDURE submit_feedback (
     p_proposals_id     IN CHAR,
     p_revision_note    IN CLOB,
     p_revision_date    IN DATE,
-    p_revision_section IN SMALLINT
+    p_revision_section IN SMALLINT,
+    p_new_status       IN VARCHAR2
 ) AS
 BEGIN
     -- Masukkan feedback dosen ke tabel feedback
@@ -44,7 +69,26 @@ BEGIN
         p_revision_section
     );
     
+    IF p_new_status NOT IN ('DISETUJUI', 'DITOLAK') THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Status tidak valid.');
+    END IF;
+    
+    UPDATE proposals
+    SET status = p_new_status
+    WHERE id = p_proposals_id;
+
     -- Commit perubahan
     COMMIT;
 END submit_feedback;
 /
+
+BEGIN
+    submit_feedback(
+        p_user_id          => 5,
+        p_proposals_id     => 'prop-001',
+        p_revision_note    => 'Tim memerlukan perbaikan pada bagian C.',
+        p_revision_date    => SYSDATE,
+        p_revision_section => 1,
+        p_new_status       => 'DISETUJUI'
+    );
+END;
